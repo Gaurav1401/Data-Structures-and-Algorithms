0ba7102c83c0ebf3e715b98962d58c9700e46b9b
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-26 13:26:32 +0530

    Q4.cpp

---
+++ Arrays/GFG Questions/Q4.cpp
@@ -0,0 +1,6 @@
+// You are given an array arr(0-based indexing). The size of the array is given by n.
+// You need to update an element at the given index. The arr[i] of the array is initially set to i+1.
+
+void updateArray(int arr[], int index, int element){
+    arr[index] = element;
+}
