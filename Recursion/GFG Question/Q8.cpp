28d03d739e45aa6a3a81d9b38f710c65e7636d85
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-29 13:34:59 +0530

    Q8.cpp

---
+++ Recursion/GFG Question/Q8.cpp
@@ -0,0 +1,12 @@
+// You are given two numbers n and r. You need to find nCr.
+// nCr = (n!) / ((n-r)!*(r!))
+// In recursive way, we can write nCr as nCr = (n-1)C(r-1) + (n-1)Cr
+
+int nCr(int n,int r)
+{
+    if (n==r || r==0)
+    {
+        return 1;
+    }
+    return nCr(n-1, r-1) + nCr(n-1, r);
+}
