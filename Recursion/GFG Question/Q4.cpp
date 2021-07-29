9c6aaa0d6a64be7701df3e065efc2e57321386ba
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-29 13:03:44 +0530

    Q4.cpp

---
+++ Recursion/GFG Question/Q4.cpp
@@ -0,0 +1,10 @@
+// You are given a number n. You need to find the sum of digits of n.
+
+int sumOfDigits(int n)
+{
+    if (n<10)
+    {
+        return n;
+    }
+    return sumOfDigits(n/10) + n%10;
+}
