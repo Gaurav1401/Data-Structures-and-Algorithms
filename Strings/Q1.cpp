f5cdd14c36d76bd2a612cbb32525be2a30a14271
Gaurav1401 <gk.bhagat14@gmail.com>
2021-08-02 21:44:12 +0530

    Q1.cpp

---
+++ Strings/Q1.cpp
@@ -0,0 +1,9 @@
+// You are given a string s. You need to find the length of the string.
+
+lass Solution{
+  public:
+    int lengthString(string str)
+    {
+        return str.length();
+    }
+};
