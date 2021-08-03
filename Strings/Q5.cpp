42949d9f0290b9e2e4bbb5c083f293cce0f102b2
Gaurav1401 <gk.bhagat14@gmail.com>
2021-08-03 14:06:14 +0530

    Q5.cpp

---
+++ Strings/Q5.cpp
@@ -0,0 +1,14 @@
+// You are given a string s. You need to convert the case of lowercase letter to uppercase letters.
+
+string caseConversion(string str){
+    
+    for (int i=0; i<str.length(); i++)
+    {
+        if((str[i] >= 97) && (str[i] <= 122)) // considering lowercase letters only
+            str[i] -= 32;
+        else
+            continue;
+    }
+    
+    return str;
+}
