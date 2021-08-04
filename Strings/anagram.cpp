d2da1a30302f98fb46acdccdfd505233962ededf
Gaurav1401 <gk.bhagat14@gmail.com>
2021-08-04 22:13:26 +0530

    anagram question

--- Strings/anagram.cpp
+++ Strings/anagram.cpp
@@ -1,3 +1,8 @@
+// Given two strings a and b consisting of lowercase characters. 
+// The task is to check whether two given strings are an anagram of each other or not. 
+// An anagram of a string is another string that contains the same characters, 
+// only the order of characters can be different. For example, “act” and “tac” are an anagram of each other.
+
 class Solution
 {
 public:
