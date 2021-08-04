4b5657c2d3466281e55e4f5f76be92692790c268
Gaurav1401 <gk.bhagat14@gmail.com>
2021-08-04 21:42:35 +0530

    string_reverse

---
+++ Strings/string_reverse.cpp
@@ -0,0 +1,15 @@
+string reverseWord(string str) {
+    int start = 0;
+    int end = str.size() - 1;
+    char temp;
+    while (start <= end)
+    {
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }return str;
+
+}
